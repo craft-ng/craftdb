819f93a2a60553807419412a349c65e294da1fc6
paulrb <ppbalaga@gmail.com>
2016-06-29 21:27:45 +0200

    Implemented npm script: build - that compiles documents but does not initialize watcher and/or server

---
+++ docsite/bin/build-execute.js
@@ -0,0 +1,5 @@
+'use strict';
+
+var _build = require('./build');
+
+(0, _build.build)(_build.config); //This script is used as npm script
---
+++ docsite/es6/build-execute.es6
@@ -0,0 +1,5 @@
+//This script is used as npm script
+
+import {build, config} from './build';
+
+build(config);
