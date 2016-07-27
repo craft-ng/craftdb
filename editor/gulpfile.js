f6367ba6ffa160a0f82edd73857ac023835adcfa
paulrb <ppbalaga@gmail.com>
2016-07-27 22:19:58 +0200

    Configured gulp tasks: clean, copy-scripts with proper Promise/async handling

--- editor/gulpfile.js
+++ editor/gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
+var gulpDebug = require('gulp-debug');
 var path = require('path');
+var del = require('del');
 var es = require('event-stream');
 var ts = require('gulp-typescript');
 var tsProject = ts.createProject('tsconfig.json');
@@ -15,28 +17,42 @@ var config = {
     clientStylesDirectory: '.www/styles'
 };
 
+function copy(files, destination) {
+    return gulp.src(files)
+        .pipe(gulp.dest(destination));
+}
+
+gulp.task('clean', function () {
+    var directoriesStartingWithDot = './.**/';
+    return del(directoriesStartingWithDot);
+});
+
 gulp.task('typescript', function () {
     return tsProject.src()
         .pipe(ts(tsProject))
         .js.pipe(gulp.dest(config.tempDirectory));
 });
 
-gulp.task('copy-scripts', function () {
-    function copy(files, destination) {
-        return gulp.src(path.join(config.tempDirectory, files))
-            .pipe(gulp.dest(destination));
-    }
-
-    return es.merge(
-        copy('./src/server/**/*.js', config.serverScriptsDirectory),
-        copy('./src/client/**/*.js', config.clientScriptsDirectory)
+gulp.task('copy-scripts-client', function () {
+    return copy(
+        path.join(config.tempDirectory, './src/server/**/*.js'),
+        config.serverScriptsDirectory
     );
 });
 
-gulp.task('stylus', function () {
-    return gulp.src('./assets/**/*.styl')
-        .pipe(stylus())
-        .pipe(gulp.dest(config.clientScriptsDirectory));
+gulp.task('copy-scripts-server', function () {
+    return copy(
+        path.join(config.tempDirectory, './src/client/**/*.js'),
+        config.clientScriptsDirectory
+    );
 });
 
-gulp.task('develop', gulp.series(gulp.parallel('typescript', 'stylus'), 'copy-scripts'));
+gulp.task('copy-scripts', gulp.parallel('copy-scripts-server', 'copy-scripts-client'));
+
+gulp.task('stylus', function () {
+    return gulp.src('./styles/**/*.styl')
+        .pipe(stylus())
+        .pipe(gulp.dest(config.clientStylesDirectory));
+});
+
+gulp.task('develop', gulp.series('clean', gulp.parallel('typescript', 'stylus'), 'copy-scripts'));
