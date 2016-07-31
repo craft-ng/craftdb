057438911b0ffa534b6e29e6e88ad0a8b76a5035
paulrb <ppbalaga@gmail.com>
2016-07-31 22:34:00 +0200

    Setup simple routes with koa-router

--- editor/.dist/app.js
+++ editor/.dist/app.js
@@ -8,15 +8,23 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 const Koa = require("koa");
-// const KoaRouter = require('koa-router');
+const KoaRouter = require('koa-router');
 const views = require('koa-views');
 const app = new Koa();
-// const router = new KoaRouter();
+const router = new KoaRouter();
+router
+    .get('/', (ctx, next) => __awaiter(this, void 0, void 0, function* () {
+    yield ctx.render('user/profile');
+}))
+    .get('/users', (ctx, next) => {
+    ctx.body = 'User list to show up here';
+});
 app.use(views(__dirname + '/views', {
     map: { pug: 'pug' },
     extension: 'pug'
 }));
-app.use((ctx, next) => __awaiter(this, void 0, void 0, function* () {
-    yield ctx.render('user/profile');
-}));
+app.use(router.routes());
+// app.use(async(ctx:Koa.Context, next:Function) => {
+//     await ctx.render('user/profile');
+// });
 app.listen(3010);
--- editor/src/server/app.ts
+++ editor/src/server/app.ts
@@ -1,17 +1,23 @@
 import Koa = require("koa");
-// const KoaRouter = require('koa-router');
+const KoaRouter = require('koa-router');
 const views = require('koa-views');
 
 const app:Koa = new Koa();
-// const router = new KoaRouter();
+const router = new KoaRouter();
+
+router
+    .get('/', async(ctx:Koa.Context, next:Function)=> {
+        await ctx.render('user/profile');
+    })
+    .get('/users', (ctx:Koa.Context, next:Function)=> {
+        ctx.body = 'User list to show up here';
+    });
 
 app.use(views(__dirname + '/views', {
     map: {pug: 'pug'},
     extension: 'pug'
 }));
 
-app.use(async(ctx:Koa.Context, next:Function) => {
-    await ctx.render('user/profile');
-});
+app.use(router.routes());
 
 app.listen(3010);
