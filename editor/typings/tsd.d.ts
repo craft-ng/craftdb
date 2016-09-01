57c2917afe6b2f54c2d4853cd6cd8db474f0a08e
paulrb <ppbalaga@gmail.com>
2016-09-01 22:14:29 +0200

    Installed glob and extend packages from npm and tsd

--- editor/typings/tsd.d.ts
+++ editor/typings/tsd.d.ts
@@ -3,3 +3,6 @@
 /// <reference path="node/node.d.ts" />
 /// <reference path="cookies/cookies.d.ts" />
 /// <reference path="koa-mount/koa-mount.d.ts" />
+/// <reference path="extend/extend.d.ts" />
+/// <reference path="glob/glob.d.ts" />
+/// <reference path="minimatch/minimatch.d.ts" />
